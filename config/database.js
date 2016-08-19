819a1ad8c24e461b9b3687c57e2acc717792623b
albertosouza <alberto.souza.99@gmail.com>
2016-08-19 05:03:22 -0300

    update redis and database configs

--- config/database.js
+++ config/database.js
@@ -1,14 +1,3 @@
-const session = require('express-session');
-const RedisStore = require('connect-redis')(session);
-const redis = require('redis');
-
-// change host and port to your redis cfgs:
-
-//CREATE REDIS CLIENT
-const redisClient = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT
-});
 
 var configs = {
   database: {
@@ -38,6 +27,18 @@ var configs = {
 }
 
 if (process.env.REDIS_HOST) {
+  const session = require('express-session');
+  const RedisStore = require('connect-redis')(session);
+  const redis = require('redis');
+
+  // change host and port to your redis cfgs:
+
+  //CREATE REDIS CLIENT
+  const redisClient = redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT
+  });
+
   configs.session = {
     secret: process.env.REDIS_SECRET || '1312313103310391803',
     store: new RedisStore({
@@ -46,7 +47,7 @@ if (process.env.REDIS_HOST) {
       host: process.env.REDIS_HOST,
       port: process.env.REDIS_PORT,
       ttl: 31557600, // 1 year, this is set by secconds
-      db: process.env.REDIS_DB
+      db: Number(process.env.REDIS_DB) || 10
     }),
     resave: false, // don't save session if unmodified
     saveUninitialized: false
